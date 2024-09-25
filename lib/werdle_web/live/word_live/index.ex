02f15c93256d7fe9f24a0be6a6dfcf4ddd328e8d
Jian Lik Ng <jlik.lik@gmail.com>
2024-09-25 03:12:40 -0700

    Finished tutorial part 3

---
+++ lib/werdle_web/live/word_live/index.ex
@@ -0,0 +1,18 @@
+defmodule WerdleWeb.WordLive.Index do
+  use WerdleWeb, :live_view
+
+  alias Werdle.WordBank
+  alias Werdle.WordBank.Word
+
+  @impl true
+  def mount(_params, _session, socket) do
+
+    socket =
+      socket
+      |> assign(:cell_backgrounds, %{})
+      |> assign(:changeset, nil)
+
+    {:ok, socket}
+  end
+
+end
